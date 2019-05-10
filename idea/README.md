4055ea8859c5f8b033fdb22ea38694002be9b621
MC <mc@matteocarbone.com>
2019-05-10 11:39:27 +0200

    Fixed modules

--- idea/README.md
+++ idea/README.md
@@ -1,3 +1,3 @@
-IDEA Ionic Extra v2.1.0
+IDEA Ionic Extra v2.1.1
 
 *Note:* This **folder** is supposed to be **read-only**; all the changes should be performed over the repo **[IDEA Ionic extra](htps://github.com/uatisdeproblem/IDEA-Ionic-extra)**.
--- idea/index.ts
+++ idea/index.ts
@@ -1,12 +1,18 @@
 export * from './auth/auth.module';
-export * from './auth/auth.service';
-export * from './customFields/customFields.module';
-export * from './select/select.module';
+  export * from './auth/auth.service';
+export * from './calendar/calendar.module';
 export * from './checker/checker.module';
+export * from './customFields/customFields.module';
+export * from './languagePicker/languagePicker.module';
+export * from './list/list.module';
+export * from './mde/mde.module';
+export * from './select/select.module';
+export * from './signature/signature.module';
 
 export * from './AWSAPI.service';
+export * from './errorReporting.service';
+export * from './extBrowser.service';
 export * from './loading.service';
 export * from './message.service';
-export * from './extBrowser.service';
 export * from './tinCan.service';
-export * from './errorReporting.service';
+export * from './zebraReader.service';
