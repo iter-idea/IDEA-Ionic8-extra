18ec9e1cfb80c30b12a1d41709217bda80a16566
uatisdeproblem <mc@matteocarbone.com>
2019-10-10 22:53:54 +0200

    Updated config format

--- assets/configs/idea-config.js
+++ assets/configs/idea-config.js
@@ -1,17 +1,17 @@
-const IDEA_APP_TITLE = 'App name';
-const IDEA_APP_URL = null;
+window.IDEA_APP_TITLE = 'App name';
+window.IDEA_APP_URL = null;
 
-const IDEA_PROJECT = 'project-code';
+window.IDEA_PROJECT = 'project-code';
 
-const IDEA_API_ID = '123';
-const IDEA_API_REGION = 'eu-west-2';
-const IDEA_API_VERSION = 'dev';
+window.IDEA_API_ID = '123';
+window.IDEA_API_REGION = 'eu-west-2';
+window.IDEA_API_VERSION = 'dev';
 
-const IDEA_AWS_COGNITO_USER_POOL_ID = 'eu-west-2_XXX';
-const IDEA_AWS_COGNITO_WEB_CLIENT_ID = 'ABC';
+window.IDEA_AWS_COGNITO_USER_POOL_ID = 'eu-west-2_XXX';
+window.IDEA_AWS_COGNITO_WEB_CLIENT_ID = 'ABC';
 
-const IDEA_AUTH_REGISTRATION_POSSIBLE = false;
-const IDEA_AUTH_WEBSITE = 'https://iter-idea.com';
+window.IDEA_AUTH_REGISTRATION_POSSIBLE = false;
+window.IDEA_AUTH_WEBSITE = 'https://iter-idea.com';
 
-const IDEA_API_URL = 'https://9ssilq3yzh.execute-api.eu-west-2.amazonaws.com/prod';
-const IDEA_ERROR_REPORTING_API_URL = IDEA_API_URL + '/errorReporting';
+window.IDEA_API_URL = 'https://9ssilq3yzh.execute-api.eu-west-2.amazonaws.com/prod';
+window.IDEA_ERROR_REPORTING_API_URL = IDEA_API_URL + '/errorReporting';
