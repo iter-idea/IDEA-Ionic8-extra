7b61d707a1570c9900ac2c58506d9c8e0856781b
uatisdeproblem <mc@matteocarbone.com>
2019-10-21 18:25:55 +0200

    Improved offline

--- idea/offline/offline.service.ts
+++ idea/offline/offline.service.ts
@@ -7,9 +7,9 @@ import { IDEAAWSAPIService, CacheModes, APIRequestOption } from '../AWSAPI.servi
 import { epochDateTime } from 'idea-toolbox';
 
 /**
- * After how much time the offline cached content needs a refresh.
+ * After how much time we need to request a synchronisation to keep the contents up-to-date.
  */
-export const SYNC_EXPIRATION_INTERVAL: number = 1000 * 60 * 60 * 24; // a day
+export const SYNC_EXPIRATION_INTERVAL = 1000 * 60 * 60 * 24; // a day
 /**
  * Prefix of an online resource name, for the local storage.
  */
@@ -18,6 +18,11 @@ export const QUEUE_API_REQUESTS_KEY = 'IDEAOfflineService.queueAPIrequests';
  * Id of the last sync time info in the local storage.
  */
 export const LAST_SYNC_KEY = 'IDEAOfflineService.lastSyncAt';
+/**
+ * The max number of element to cache to automatically start the synchronisation.
+ * After this number, a manual confirmation is requested, to save internet data and computational resources.
+ */
+export const MAX_NUM_ELEMENTS_FOR_QUICK_SYNC = 50;
 
 /**
  * Manage the offline functioning of the app (upload/download/synchronization).
@@ -33,8 +38,8 @@ export const LAST_SYNC_KEY = 'IDEAOfflineService.lastSyncAt';
  * ```
  * // set the resources we want to download from the back-end
  * this.offline.resourcesToCache = [
- *   new CacheableResource('trainingModels', 'trainingModelId', this.t.instant('MENU.MODELS')),
- *   new CacheableResource('trainings', 'trainingId', this.t.instant('MENU.TRAININGS'))
+ *   new CacheableResource([`teams/${team.teamId}/models`], 'modelId', this.t.instant('MENU.MODELS')),
+ *   new CacheableResource([`teams/${team.teamId}/activities`], 'activityId', this.t.instant('MENU.ACTIVITIES'))
  * ];
  * this.offline.synchronizeIfNeeded();
  * ```
@@ -45,6 +50,10 @@ export class IDEAOfflineService {
    * True/false if the platform is offline/online.
    */
   public isOffline: boolean;
+  /**
+   * True/false if the platform is online/offline. Shortcut.
+   */
+  public isOnline: boolean;
   /**
    * True when running a general synchronization.
    */
@@ -57,6 +66,11 @@ export class IDEAOfflineService {
    * True if an error happened in the last general synchronization.
    */
   public errorInLastSync: boolean;
+  /**
+   * True if the synchronisation is too vast and so require a manual action of the user.
+   * Based on `MAX_NUM_ELEMENTS_FOR_QUICK_SYNC`.
+   */
+  public requiresManualConfirmation: boolean;
   /**
    * If false, ignore the entire upload scenario.
    */
@@ -72,20 +86,30 @@ export class IDEAOfflineService {
 
   constructor(protected storage: Storage, protected t: TranslateService, protected API: IDEAAWSAPIService) {
     this.isOffline = !navigator.onLine;
+    this.isOnline = navigator.onLine;
     window.addEventListener('online', () => {
       this.isOffline = false;
+      this.isOnline = true;
       // once back online, try a synchronization if needed or if the last one failed
       if (this.errorInLastSync) this.synchronize();
       else this.synchronizeIfNeeded();
     });
-    window.addEventListener('offline', () => (this.isOffline = true));
+    window.addEventListener('offline', () => {
+      this.isOffline = true;
+      this.isOnline = false;
+    });
     this.synchronizing = false;
     this.errorInLastSync = false;
+    this.requiresManualConfirmation = false;
     this.useQueueAPIRequests = true;
     this.queueAPIRequests = new Array<APIRequest>();
     this.resourcesToCache = new Array<CacheableResource>();
     this.storage.get(LAST_SYNC_KEY).then((lastSyncAt: number) => (this.lastSyncAt = lastSyncAt || 0));
   }
+
+  //
+  // UPLOAD
+  //
 
   /**
    * Execute all the API requests in the queue; the requests terminated with an error will
@@ -169,57 +193,40 @@ export class IDEAOfflineService {
     }
   }
 
+  //
+  // DOWNLOAD
+  //
+
   /**
-   * Analize the resource by consulting its list as an index, then cache the latter.
-   * Based on the `mAt` mechanism, identify the new/changed elements and cache them.
-   * @todo old elements in cache aren't deleted (they just disappear from the index).
+   * Analize the resource by consulting its list as an index, and gather the elements to cache (new/changed),
+   * based on the `mAt` mechanism.
+   * @return the index and the elements to cache
    */
-  protected cacheResource(resource: CacheableResource): Promise<void> {
-    return new Promise((resolve, reject) => {
+  protected analyseResource(resource: CacheableResource): Promise<void> {
+    return new Promise(resolve => {
       resource.synchronizing = true;
       resource.error = false;
-      // prepare the array of ids of the elements to cache
-      const toCache = new Array<string>();
-      // get the resource elements ONLINE (list)
+      // get the resource elements ONLINE (list), for each resource path
       Async.forEach(
-        resource.names,
-        (name, doneName) => {
-          this.API.getResource(name, { useCache: CacheModes.NO_CACHE }).then((cloudElements: Array<any>) => {
+        resource.resourcePaths,
+        (path, donePath) => {
+          this.API.getResource(path, { useCache: CacheModes.NO_CACHE }).then((cloudElements: Array<any>) => {
             // get the resource elements OFFLINE (list)
-            this.API.getResource(name, { useCache: CacheModes.CACHE_ONLY }).then((localElements: Array<any>) => {
+            this.API.getResource(path, { useCache: CacheModes.CACHE_ONLY }).then((localElements: Array<any>) => {
               // prepare the list, keeping the most updated elements between cloud and local version
-              const mostUpdatedElements = cloudElements.map(cloudEl => {
+              resource.index[path] = cloudElements.map(cloudEl => {
                 const localEl = localElements.find(l => l[resource.idAttribute] === cloudEl[resource.idAttribute]);
                 if (!localEl) {
                   // !localEl -> download cloudEl
-                  toCache.push(cloudEl[resource.idAttribute]);
+                  resource.elements[path].push(cloudEl[resource.idAttribute]);
                   return cloudEl;
                 } else if (!localEl.mAt || !cloudEl.mAt || cloudEl.mAt > localEl.mAt) {
                   // cloudEl more recent than localEl -> download cloudEl
-                  toCache.push(localEl[resource.idAttribute]);
+                  resource.elements[path].push(localEl[resource.idAttribute]);
                   return cloudEl;
                 } else return localEl;
               });
-              // cache the list: keep the most updated version (cloud/local) of each resource
-              this.API.putInCache(name, mostUpdatedElements).then(() => {
-                // cache the elements identified
-                Async.eachSeries(
-                  toCache,
-                  (id: string, done: any) => {
-                    // note: the NETWORK_FIRST request will cache the response
-                    this.API.getResource(name, { resourceId: id, useCache: CacheModes.NETWORK_FIRST })
-                      .then(() => done())
-                      .catch((err: Error) => done(err));
-                  },
-                  (err: Error) => {
-                    resource.error = Boolean(err);
-                    resource.synchronizing = false;
-                    // in case of errors, we wouldn't know which element had failed: kill the entire process
-                    if (err) reject(err);
-                    else doneName();
-                  }
-                );
-              });
+              donePath();
             });
           });
         },
@@ -227,6 +234,43 @@ export class IDEAOfflineService {
       );
     });
   }
+  /**
+   * Cache the analysed elements of a resource.
+   * @todo old elements in cache aren't deleted (they just disappear from the index).
+   */
+  protected cacheResource(resource: CacheableResource): Promise<void> {
+    return new Promise((resolve, reject) => {
+      Async.forEach(
+        resource.resourcePaths,
+        (path, donePath) => {
+          // cache the list: keep the most updated version (cloud/local) of each resource
+          this.API.putInCache(path, resource.index[path]).then(() => {
+            // cache the elements identified
+            Async.eachSeries(
+              resource.elements[path],
+              (id: string, done: any) => {
+                // note: the NETWORK_FIRST request will cache the response
+                this.API.getResource(path, { resourceId: id, useCache: CacheModes.NETWORK_FIRST })
+                  .then(() => done())
+                  .catch((err: Error) => done(err));
+              },
+              (err: Error) => {
+                // in case of errors, we wouldn't know which element had failed: kill the entire process
+                resource.error = Boolean(err);
+                resource.synchronizing = false;
+                donePath(err);
+              }
+            );
+          });
+        },
+        (err: Error) => (err ? reject(err) : resolve())
+      );
+    });
+  }
+
+  //
+  // SYNCHRONISATION (UPLOAD+DOWNLOAD)
+  //
 
   /**
    * Run general synchronization, only if needed, i.e. the cached content expired or
@@ -239,33 +283,52 @@ export class IDEAOfflineService {
   }
   /**
    * General synchronization of the whole set of resources (upload+download).
+   * @param manualConfirmation if true, the request comes directly from a user action (not automatic procedures).
    */
-  public synchronize() {
+  public synchronize(manualConfirmation?: boolean) {
     if (this.synchronizing) return;
     this.synchronizing = true;
     this.errorInLastSync = false;
+    this.requiresManualConfirmation = false;
     // try to upload the elements of all the offline resources
     this.runQueueAPIRequests()
       .then(() => {
-        // download (if needed) an updated version of the cached elements for each resource
+        // analyse the resources to see if there are updated versions of the cached elements to download
         Async.each(
           this.resourcesToCache,
-          (resource: CacheableResource, done: any) => {
-            this.cacheResource(resource)
-              .then(() => done())
-              .catch(() => {
-                // show an error alert, but don't make the entire process to fail
-                this.errorInLastSync = true;
-                done();
-              });
-          },
+          (res, done) => this.analyseResource(res).then(() => done()),
           () => {
-            if (this.errorInLastSync) this.synchronizing = false;
-            else {
-              // all the resources are succesfully in sync; update the timestamp of last sync
-              this.lastSyncAt = Date.now();
-              this.storage.set(LAST_SYNC_KEY, this.lastSyncAt).then(() => (this.synchronizing = false));
+            // count the total number of the elements to cache in between all the resources
+            let numElToCache = 0;
+            this.resourcesToCache.forEach(r => (numElToCache += r.getNumElementsToCache()));
+            // decide if to proceed with the syncronisation;
+            // in case of too many elements to update, requires a manual confirmation
+            if (numElToCache > MAX_NUM_ELEMENTS_FOR_QUICK_SYNC && !manualConfirmation) {
+              this.requiresManualConfirmation = true;
+              this.synchronizing = false;
+              return;
             }
+            // download the elements of the analysed resources
+            Async.each(
+              this.resourcesToCache,
+              (resource, done) => {
+                this.cacheResource(resource)
+                  .then(() => done())
+                  .catch(() => {
+                    // show an error alert, but don't make the entire process to fail
+                    this.errorInLastSync = true;
+                    done();
+                  });
+              },
+              () => {
+                if (this.errorInLastSync) this.synchronizing = false;
+                else {
+                  // all the resources are succesfully in sync; update the timestamp of last sync
+                  this.lastSyncAt = Date.now();
+                  this.storage.set(LAST_SYNC_KEY, this.lastSyncAt).then(() => (this.synchronizing = false));
+                }
+              }
+            );
           }
         );
       })
@@ -314,7 +377,7 @@ export class CacheableResource {
   /**
    * List of resource path (e.g. `users` or `teams/${teamId}/users`).
    */
-  public names: Array<string>;
+  public resourcePaths: Array<string>;
   /**
    * The name of the attribute that works as resourceId.
    */
@@ -331,12 +394,34 @@ export class CacheableResource {
    * True if one of the elements failed to upload.
    */
   public error: boolean;
+  /**
+   * For each resourcePath, the index of the elements to cache for the path.
+   */
+  public index: {
+    [key: string]: Array<any>;
+  };
+  /**
+   * For each resourcePath, the elements to cache for the path.
+   */
+  public elements: {
+    [key: string]: Array<string>;
+  };
 
-  constructor(names: Array<string>, idAttribute: string, description?: string) {
-    this.names = names.map((n: string) => (n ? String(n) : null));
+  constructor(resourcePaths: Array<string>, idAttribute: string, description?: string) {
+    this.resourcePaths = resourcePaths.map((x: string) => (x ? String(x) : null));
     this.idAttribute = idAttribute;
     this.description = description || name;
     this.synchronizing = false;
     this.error = false;
+    this.index = {};
+    resourcePaths.forEach((x: string) => (this.index[x] = new Array<any>()));
+    this.elements = {};
+    resourcePaths.forEach((x: string) => (this.elements[x] = new Array<string>()));
+  }
+
+  public getNumElementsToCache(): number {
+    let count = 0;
+    this.resourcePaths.forEach(p => (count += this.elements[p].length));
+    return count;
   }
 }
