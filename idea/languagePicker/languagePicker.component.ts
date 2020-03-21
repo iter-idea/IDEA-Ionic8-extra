cd8c1603e4d9d3c925d149fdba70b33672af66bb
uatisdeproblem <mc@matteocarbone.com>
2020-03-21 23:07:38 +0100

    Fixed language picker

--- idea/languagePicker/languagePicker.component.ts
+++ idea/languagePicker/languagePicker.component.ts
@@ -1,7 +1,11 @@
-import { Component, Input } from '@angular/core';
-import { AlertController, IonFab } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { AlertController, ModalController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
+import { OverlayEventDetail } from '@ionic/core';
 import { TranslateService } from '@ngx-translate/core';
+import IdeaX = require('idea-toolbox');
+
+import { IDEASuggestionsComponent } from '../select/suggestions.component';
 
 // requires two assets folders:
 //   1. flags, containing the pngs of each country's flags
@@ -13,25 +17,47 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['languagePicker.component.scss']
 })
 export class IDEALanguagePickerComponent {
-  public languages: Array<string>;
+  constructor(
+    public t: TranslateService,
+    public storage: Storage,
+    public alertCtrl: AlertController,
+    public modalCtrl: ModalController
+  ) {}
+
   /**
-   * The side where the fab list will open. Default: bottom.
+   * Show the list of languages, to choose one.
    */
-  @Input() public side: string;
-
-  constructor(public t: TranslateService, public storage: Storage, public alertCtrl: AlertController) {
-    this.side = 'bottom';
+  public pickLanguage() {
+    this.modalCtrl
+      .create({
+        component: IDEASuggestionsComponent,
+        componentProps: {
+          data: this.t
+            .getLangs()
+            .map(l => new IdeaX.Suggestion({ value: l, name: this.t.instant('LANGUAGES.'.concat(l.toUpperCase())) })),
+          searchPlaceholder: this.t.instant('IDEA.LANGUAGE_PICKER.CHANGE_LANGUAGE'),
+          sortData: true,
+          hideIdFromUI: true,
+          hideClearButton: true
+        }
+      })
+      .then(modal => {
+        modal.onDidDismiss().then((res: OverlayEventDetail) => {
+          if (res.data && res.data.value) this.changeLanguage(res.data.value);
+        });
+        modal.present();
+      });
   }
-  public ngOnInit() {
-    this.languages = this.t.getLangs();
-  }
 
-  public changeLanguage(lang: string, fab: IonFab) {
+  /**
+   * Ask for confirmation and change the language.
+   */
+  public changeLanguage(lang: string) {
     this.alertCtrl
       .create({
         header: this.t.instant('IDEA.LANGUAGE_PICKER.APP_WILL_RESTART'),
         buttons: [
-          { text: this.t.instant('COMMON.CANCEL'), role: 'cancel', handler: () => fab.close() },
+          { text: this.t.instant('COMMON.CANCEL'), role: 'cancel' },
           {
             text: this.t.instant('COMMON.CONFIRM'),
             // needed cause sometimes the language isn't update in the interface (random)
