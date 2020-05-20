2525ed4a5f6236d8627d6992e25c084c03186df4
uatisdeproblem <mc@matteocarbone.com>
2020-05-20 19:30:34 +0200

    Added custom block component

--- idea/customFields/customFields.module.ts
+++ idea/customFields/customFields.module.ts
@@ -9,11 +9,36 @@ import { IDEACustomSectionMetaComponent } from './customSectionMeta.component';
 import { IDEACustomFieldMetaComponent } from './customFieldMeta.component';
 import { IDEALabelerModule } from '../labeler/labeler.module';
 import { IDEAIconsModule } from '../icons/icons.module';
+import { IDEACustomBlockComponent } from './customBlock.component';
+import { IDEASelectModule } from '../select/select.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, IDEATranslationsModule, IDEALabelerModule, IDEAIconsModule],
-  entryComponents: [IDEACustomBlockMetaComponent, IDEACustomSectionMetaComponent, IDEACustomFieldMetaComponent],
-  exports: [IDEACustomBlockMetaComponent, IDEACustomSectionMetaComponent, IDEACustomFieldMetaComponent],
-  declarations: [IDEACustomBlockMetaComponent, IDEACustomSectionMetaComponent, IDEACustomFieldMetaComponent]
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    IDEATranslationsModule,
+    IDEALabelerModule,
+    IDEAIconsModule,
+    IDEASelectModule
+  ],
+  entryComponents: [
+    IDEACustomBlockMetaComponent,
+    IDEACustomSectionMetaComponent,
+    IDEACustomFieldMetaComponent,
+    IDEACustomBlockComponent
+  ],
+  exports: [
+    IDEACustomBlockMetaComponent,
+    IDEACustomSectionMetaComponent,
+    IDEACustomFieldMetaComponent,
+    IDEACustomBlockComponent
+  ],
+  declarations: [
+    IDEACustomBlockMetaComponent,
+    IDEACustomSectionMetaComponent,
+    IDEACustomFieldMetaComponent,
+    IDEACustomBlockComponent
+  ]
 })
 export class IDEACustomFieldsModule {}
