4693883eca24fa27cd2389e35f67b57f374fe068
Guido <gmazza104@gmail.com>
2020-07-16 11:51:47 +0200

    Update Push Notifications Service use only FCM
    Note: Papaya needs to update SNS / Firebase configuration before to make the change (https://github.com/uatisdeproblem/papaya/issues/127)

--- idea/pushNotifications.service.ts
+++ idea/pushNotifications.service.ts
@@ -4,10 +4,6 @@ import { Plugins, PushNotificationToken, PushNotification } from '@capacitor/cor
 import { Observable } from 'rxjs';
 import { IDEAErrorReportingService } from './errorReporting.service';
 import IdeaX = require('idea-toolbox');
-
-// from idea-config.js
-declare const IDEA_API_VERSION: string;
-
 /**
  * To subscribe push notifications events.
  */
@@ -34,11 +30,7 @@ export class IDEAPushNotificationsService {
         observer.next(
           new IdeaX.PushNotificationsDevice({
             token: token.value,
-            platform: this.platform.is('ios')
-              ? IDEA_API_VERSION === 'dev'
-                ? IdeaX.PushNotificationsPlatforms.APNS_SANDBOX
-                : IdeaX.PushNotificationsPlatforms.APNS
-              : IdeaX.PushNotificationsPlatforms.FCM
+            platform: IdeaX.PushNotificationsPlatforms.FCM
           })
         );
       });
@@ -67,6 +59,13 @@ export class IDEAPushNotificationsService {
    * To fire the registration of a new device.
    */
   public registerDevice() {
-    Plugins.PushNotifications.register();
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    Plugins.PushNotifications.requestPermission().then(result => {
+      if (result.granted) {
+        Plugins.PushNotifications.register();
+      } else console.log('Notification Permission denied');
+    });
   }
 }
