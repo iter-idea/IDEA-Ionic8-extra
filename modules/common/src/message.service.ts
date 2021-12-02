0a37183a6851c7f3b0f574a06ac7d60f3d0c3db8
Matteo Carbone <mc@matteocarbone.com>
2021-12-02 11:47:55 +0100

    refactor(common): message service

--- modules/common/src/message.service.ts
+++ modules/common/src/message.service.ts
@@ -3,52 +3,52 @@ import { ToastController } from '@ionic/angular';
 
 import { IDEATranslationsService } from './translations/translations.service';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class IDEAMessageService {
-  constructor(public toastCtrl: ToastController, public t: IDEATranslationsService) {}
+  constructor(private toastCtrl: ToastController, private t: IDEATranslationsService) {}
 
   /**
    * Show a generic message toast.
    * @param message message to show
    * @param color Ionic colors defined in the theme
    */
-  protected show(message: string, color: string, dontTranslate: boolean) {
-    this.toastCtrl
-      .create({
-        message: dontTranslate ? message : this.t._(message),
-        duration: 3000,
-        position: 'bottom',
-        color,
-        buttons: [{ text: 'X', role: 'cancel' }]
-      })
-      .then(toast => toast.present());
+  private async show(message: string, color: string, dontTranslate: boolean): Promise<void> {
+    message = dontTranslate ? message : this.t._(message);
+
+    const duration = 3000;
+    const position = 'bottom';
+    const buttons = [{ text: 'X', role: 'cancel' }];
+
+    const toast = await this.toastCtrl.create({ message, duration, position, color, buttons });
+    return await toast.present();
   }
+
   /**
    * Show an info message toast.
    * @param message message to show
    */
-  public info(message: string, dontTranslate?: boolean) {
-    this.show(message, 'dark', dontTranslate);
+  async info(message: string, dontTranslate?: boolean): Promise<void> {
+    return await this.show(message, 'dark', dontTranslate);
   }
   /**
    * Show a success message toast.
    * @param message message to show
    */
-  public success(message: string, dontTranslate?: boolean) {
-    this.show(message, 'success', dontTranslate);
+  async success(message: string, dontTranslate?: boolean): Promise<void> {
+    return await this.show(message, 'success', dontTranslate);
   }
   /**
    * Show an error message toast.
    * @param message message to show
    */
-  public error(message: string, dontTranslate?: boolean) {
-    this.show(message, 'danger', dontTranslate);
+  async error(message: string, dontTranslate?: boolean): Promise<void> {
+    return await this.show(message, 'danger', dontTranslate);
   }
   /**
    * Show an warning message toast.
    * @param message message to show
    */
-  public warning(message: string, dontTranslate?: boolean) {
-    this.show(message, 'warning', dontTranslate);
+  async warning(message: string, dontTranslate?: boolean): Promise<void> {
+    return await this.show(message, 'warning', dontTranslate);
   }
 }
