9a9fc46d40a83b3a9317ffb798c7c12e46d517c4
Matteo Carbone <mc@matteocarbone.com>
2021-12-02 11:47:45 +0100

    fix(common): loading message disappearing in newer versions of angular/ionic

--- modules/common/src/loading.service.ts
+++ modules/common/src/loading.service.ts
@@ -3,41 +3,33 @@ import { LoadingController } from '@ionic/angular';
 
 import { IDEATranslationsService } from './translations/translations.service';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class IDEALoadingService {
-  protected loadingElement: HTMLIonLoadingElement;
+  private loadingElement: HTMLIonLoadingElement;
 
-  constructor(public loadingCtrl: LoadingController, public t: IDEATranslationsService) {}
+  constructor(private loadingCtrl: LoadingController, private t: IDEATranslationsService) {}
 
   /**
    * Show a loading animation.
    * @param content loading message
    */
-  public async show(content?: string): Promise<void> {
-    await this.hide();
-    this.loadingElement = await this.loadingCtrl.create({
-      message: content || this.t._('IDEA_COMMON.LOADING.PLEASE_WAIT')
-    });
+  async show(content?: string): Promise<void> {
+    const message = content || this.t._('IDEA_COMMON.LOADING.PLEASE_WAIT');
+    this.loadingElement = await this.loadingCtrl.create({ message });
     return await this.loadingElement.present();
   }
   /**
    * Change the content of the loading animation, while it's already on.
    * @param content new loading message
    */
-  public setContent(content: string) {
+  setContent(content: string): void {
     if (this.loadingElement) this.loadingElement.textContent = content;
   }
   /**
-   * Hide a previous loading animation.
-   * Retries three times if doesn't find a loader to hide (it may not be ready yet).
-   * @param retry how many times should retry before giving up
+   * Hide the loading animation.
    */
-  public async hide(retry?: number): Promise<boolean> {
-    if (!this.loadingElement) {
-      retry = retry !== undefined ? retry : 3;
-      if (retry > 0) setTimeout(() => this.hide(retry - 1), 1000);
-      return null;
-    }
-    return await this.loadingElement.dismiss();
+  async hide(): Promise<boolean> {
+    if (this.loadingElement) return await this.loadingElement.dismiss();
+    else return false;
   }
 }
