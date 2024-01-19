d1805bf5b47914a6c8483a7d48841bb9497d0762
Matteo Carbone <mc@matteocarbone.com>
2024-01-19 12:22:14 +0100

    chore: moved to angular v17

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,13 +10,14 @@ module.exports = {
         node: true
       },
       parserOptions: {
-        project: 'tsconfig.json',
+        project: ['tsconfig.json'],
         sourceType: 'module'
       },
       extends: ['plugin:@angular-eslint/recommended', 'plugin:@angular-eslint/template/process-inline-templates'],
       rules: {
         '@angular-eslint/component-selector': ['error', { type: 'element', style: 'kebab-case' }],
         '@angular-eslint/component-class-suffix': ['error', { suffixes: ['Page', 'Component'] }],
+        '@angular-eslint/directive-selector': ['error', { type: 'attribute', style: 'camelCase' }],
         '@angular-eslint/no-output-native': 0
       }
     },
