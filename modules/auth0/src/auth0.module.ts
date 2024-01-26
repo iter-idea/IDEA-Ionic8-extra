f30c254e38bd636704e47d5603e8527c8a53dabe
Matteo Carbone <mc@matteocarbone.com>
2024-01-26 15:09:37 +0100

    fix(auth0): environment parameters injected dynamically https://github.com/auth0/auth0-angular?tab=readme-ov-file#dynamic-configuration

--- modules/auth0/src/auth0.module.ts
+++ modules/auth0/src/auth0.module.ts
@@ -1,19 +1,22 @@
-import { NgModule, inject } from '@angular/core';
-import { AuthModule } from '@auth0/auth0-angular';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { AuthModule, AuthClientConfig } from '@auth0/auth0-angular';
 import { IDEAEnvironment } from '@idea-ionic/common';
-const env = inject(IDEAEnvironment); // @todo to check
 
-@NgModule({
-  imports: [
-    AuthModule.forRoot({
+const injectConfig = (env: any, config: AuthClientConfig): (() => void) => {
+  return (): void =>
+    config.set({
       domain: env.auth0.domain,
       clientId: env.auth0.clientId,
       useRefreshTokens: env.auth0.storeRefreshToken,
       cacheLocation: env.auth0.storeRefreshToken ? 'localstorage' : 'memory',
-      authorizationParams: {
-        redirect_uri: env.auth0.callbackUri || window.location.origin
-      }
-    })
+      authorizationParams: { redirect_uri: env.auth0.callbackUri || window.location.origin }
+    });
+};
+
+@NgModule({
+  imports: [AuthModule.forRoot()],
+  providers: [
+    { provide: APP_INITIALIZER, useFactory: injectConfig, deps: [IDEAEnvironment, AuthClientConfig], multi: true }
   ]
 })
 export class IDEAAuth0Module {}
