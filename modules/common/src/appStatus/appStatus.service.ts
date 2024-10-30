7752b7fef47097c7fe6212f39476946c98b0fd65
Matteo Carbone <mc@matteocarbone.com>
2024-10-30 18:06:29 +0000

    feat(common): AppStatus via Asset (vs via Api)

--- modules/common/src/appStatus/appStatus.service.ts
+++ modules/common/src/appStatus/appStatus.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { NavController, ToastController } from '@ionic/angular';
-import { AppStatus } from 'idea-toolbox';
+import { AppStatus, markdown } from 'idea-toolbox';
 
 import { IDEAEnvironment } from '../../environment';
 import { IDEATranslationsService } from '../translations/translations.service';
@@ -19,29 +19,44 @@ export class IDEAAppStatusService {
   private _api = inject(IDEAApiService);
   private _storage = inject(IDEAStorageService);
 
-  storageKey: string;
-
   appStatus: AppStatus;
+
+  storageKey: string;
+  statusFileURL: string;
 
   constructor() {
     this.storageKey == (this._env.idea.project || 'app').concat('_LAST_MESSAGE');
+    this.statusFileURL = `${window.location.hostname === 'localhost' ? '' : window.location.origin}/assets/status.json`;
   }
 
   /**
    * Check the app's status and take according actions.
    */
-  async check(options: { toastColor?: string; toastPosition?: string } = {}): Promise<AppStatus> {
-    const appStatus = await this.getStatus();
-
+  async check(options: { viaApi?: boolean; toastColor?: string; toastPosition?: string } = {}): Promise<AppStatus> {
+    const appStatus = await this.getStatus(options.viaApi);
     if (appStatus.inMaintenance || appStatus.mustUpdate) await this._nav.navigateRoot(['app-status']);
     else await this.presentToast(appStatus, { color: options.toastColor, position: options.toastPosition });
-
     return appStatus;
   }
-  private async getStatus(): Promise<AppStatus> {
+  private async getStatus(viaApi = false): Promise<AppStatus> {
     if (this.appStatus) return this.appStatus;
-    this.appStatus = new AppStatus(await this._api.getResource(['status']));
+    this.appStatus = await (viaApi ? this.getStatusFromApi() : this.getStatusFromAsset());
     return this.appStatus;
+  }
+  private async getStatusFromApi(): Promise<AppStatus> {
+    return new AppStatus(await this._api.getResource(['status']));
+  }
+  private async getStatusFromAsset(): Promise<AppStatus> {
+    const res = await fetch(this.statusFileURL, { method: 'GET', cache: 'no-cache' });
+    if (res.status !== 200) throw new Error('Status not found');
+    const statusFromS3: InternalAppVersionStatusViaAsset = await res.json();
+    return new AppStatus({
+      version: this._env.idea.app.version,
+      inMaintenance: statusFromS3.maintenance,
+      mustUpdate: statusFromS3.minVersion ? statusFromS3.minVersion > this._env.idea.app.version : false,
+      content: statusFromS3.messages[this._env.idea.app.version],
+      latestVersion: statusFromS3.latestVersion
+    });
   }
   private async presentToast(appStatus: AppStatus, options: { color?: string; position?: string } = {}): Promise<void> {
     let message = appStatus.content || '';
@@ -63,4 +78,23 @@ export class IDEAAppStatusService {
     const toast = await this._toast.create({ message, buttons, position, color });
     await toast.present();
   }
+}
+
+interface InternalAppVersionStatusViaAsset {
+  /**
+   * Whether the app is in maintenance mode.
+   */
+  maintenance: boolean;
+  /**
+   * The latest version of the app.
+   */
+  latestVersion: string;
+  /**
+   * The minimum required version to access the app.
+   */
+  minVersion: string;
+  /**
+   * The optional messages for each of the app's versions.
+   */
+  messages: { [version: string]: markdown };
 }
