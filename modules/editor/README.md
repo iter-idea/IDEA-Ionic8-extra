5cb5c449f1bde26b59a21a71b82a9f8ca877012e
Matteo Carbone <uatisdeproblem@users.noreply.github.com>
2025-08-14 13:29:04 +0100

    fix(editor): update to full compatibility with Angular v20

--- modules/editor/README.md
+++ modules/editor/README.md
@@ -16,3 +16,30 @@ To make sure the translations are loaded into the project:
 
 - add a reference to this module in the environment variable `idea.ionicExtraModules` (see `environment.ts`);
 - copy the `i18n/editor` folder (you can filter the languages you need) of the module in the project's `assets/i18n` folder.
+
+## List of available SCSS variables for customisation
+
+```scss
+:root {
+  --idea-html-editor-background-color: var(--ion-color-light);
+  --idea-html-editor-border-color: var(--ion-color-light-shade);
+  --idea-html-editor-color: var(--ion-color-light-contrast);
+  --idea-html-editor-toolbar-background-color: var(--ion-color-light);
+  --idea-html-editor-toolbar-color: var(--ion-color-light-contrast);
+  --idea-html-editor-toolbar-border-color: var(--ion-color-light);
+  --idea-html-editor-button-background-color: var(--ion-color-light);
+  --idea-html-editor-button-border-color: var(--ion-color-light);
+  --idea-html-editor-button-color: var(--ion-color-light-contrast);
+  --idea-html-editor-button-focused: var(--ion-color-medium);
+  --idea-html-editor-button-selected: var(--ion-color-medium-tint);
+
+  --idea-html-editor-view-margin: 0;
+  --idea-html-editor-view-padding: 20px;
+  --idea-html-editor-view-background-color: var(--ion-color-white);
+  --idea-html-editor-view-color: var(--ion-color-white-contrast);
+  --idea-html-editor-view-box-shadow: none;
+  --idea-html-editor-view-border-width: 1px;
+  --idea-html-editor-view-border-color: var(--ion-border-color);
+  --idea-html-editor-view-border-radius: 0;
+}
+```
--- modules/editor/src/htmlEditor.component.ts
+++ modules/editor/src/htmlEditor.component.ts
@@ -9,10 +9,12 @@ import {
   SimpleChanges,
   inject,
   SecurityContext,
-  ViewChild
+  ViewChild,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef
 } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
-import { AlertController } from '@ionic/angular/standalone';
+import { AlertController, IonIcon } from '@ionic/angular/standalone';
 import { AngularEditorComponent, AngularEditorConfig, AngularEditorModule } from '@kolkov/angular-editor';
 // @ts-ignore
 import { docsSoap } from 'docs-soap';
@@ -20,8 +22,9 @@ import { IDEAMessageService, IDEATranslatePipe, IDEATranslationsService } from '
 
 @Component({
   standalone: true,
-  imports: [FormsModule, AngularEditorModule, IDEATranslatePipe],
+  imports: [FormsModule, AngularEditorModule, IDEATranslatePipe, IonIcon],
   selector: 'idea-html-editor',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     @if (editMode) {
       <angular-editor
@@ -29,17 +32,19 @@ import { IDEAMessageService, IDEATranslatePipe, IDEATranslationsService } from '
         [config]="editorConfig"
         (dragover)="onDragOver($event)"
         (drop)="onDrop($event)"
-        (input)="contentChange.emit($event.target.innerHTML)"
         (paste)="cleanHTMLCode()"
         [(ngModel)]="text"
+        (ngModelChange)="onTextChange($event)"
       >
         <ng-template #customButtons>
           <ae-toolbar-set>
-            <ae-button
-              iconClass="fa fa-arrows-h"
+            <button
+              aeButton
               [title]="'IDEA_EDITOR.SELECT_SIZE_FOR_IMAGE' | translate"
-              (buttonClick)="askAndApplySizeToSelectedImages()"
-            />
+              (click)="askAndApplySizeToSelectedImages()"
+            >
+              <ion-icon icon="resize" />
+            </button>
           </ae-toolbar-set>
         </ng-template>
       </angular-editor>
@@ -49,54 +54,96 @@ import { IDEAMessageService, IDEATranslatePipe, IDEATranslationsService } from '
   `,
   styles: [
     `
+      ::ng-deep :root {
+        --idea-html-editor-background-color: var(--ion-color-light);
+        --idea-html-editor-border-color: var(--ion-color-light-shade);
+        --idea-html-editor-color: var(--ion-color-light-contrast);
+        --idea-html-editor-toolbar-background-color: var(--ion-color-light);
+        --idea-html-editor-toolbar-color: var(--ion-color-light-contrast);
+        --idea-html-editor-toolbar-border-color: var(--ion-color-light);
+        --idea-html-editor-button-background-color: var(--ion-color-light);
+        --idea-html-editor-button-border-color: var(--ion-color-light);
+        --idea-html-editor-button-color: var(--ion-color-light-contrast);
+        --idea-html-editor-button-focused: var(--ion-color-medium);
+        --idea-html-editor-button-selected: var(--ion-color-medium-tint);
+
+        --idea-html-editor-view-margin: 0;
+        --idea-html-editor-view-padding: 20px;
+        --idea-html-editor-view-background-color: var(--ion-color-white);
+        --idea-html-editor-view-color: var(--ion-color-white-contrast);
+        --idea-html-editor-view-box-shadow: none;
+        --idea-html-editor-view-border-width: 1px;
+        --idea-html-editor-view-border-color: var(--ion-border-color);
+        --idea-html-editor-view-border-radius: 0;
+      }
+    `,
+    `
+      ::ng-deep {
+        .angular-editor-wrapper {
+          background-color: transparent !important;
+        }
+        .angular-editor-toolbar {
+          border-color: var(--idea-html-editor-border-color) !important;
+          background-color: var(--idea-html-editor-toolbar-background-color) !important;
+          position: sticky;
+          top: 0;
+          z-index: 1000;
+        }
+        .ae-picker-options,
+        .ae-picker-label,
+        .ae-picker-label:before {
+          background: var(--idea-html-editor-toolbar-background-color) !important;
+          background-color: var(--idea-html-editor-toolbar-background-color) !important;
+          border-color: var(--idea-html-editor-toolbar-border-color) !important;
+          color: var(--idea-html-editor-toolbar-color) !important;
+        }
+        .angular-editor-toolbar svg {
+          fill: currentColor !important;
+        }
+        .angular-editor-toolbar #indent-,
+        .angular-editor-toolbar #outdent- {
+          stroke: currentColor !important;
+        }
+        .angular-editor-toolbar .ae-picker-options .ae-picker-item.focused,
+        .ae-picker-options .ae-picker-item:hover {
+          background-color: var(--idea-html-editor-button-focused) !important;
+          color: var(--idea-html-editor-toolbar-color) !important;
+        }
+        .ae-picker-options .ae-picker-item.selected {
+          background-color: var(--idea-html-editor-button-selected) !important;
+          color: var(--idea-html-editor-toolbar-color) !important;
+        }
+        .angular-editor-button {
+          background-color: var(--idea-html-editor-button-background-color) !important;
+          border-color: var(--idea-html-editor-button-border-color) !important;
+          color: var(--idea-html-editor-button-color) !important;
+        }
+        .angular-editor-textarea {
+          background-color: var(--idea-html-editor-background-color) !important;
+          border-color: var(--idea-html-editor-border-color) !important;
+          color: var(--idea-html-editor-color) !important;
+        }
+      }
       ::ng-deep:has(+ #foregroundColorPicker-),
       ::ng-deep:has(+ #backgroundColorPicker-) {
         display: inline !important;
         position: absolute;
         visibility: hidden;
       }
-      ::ng-deep {
-        .angular-editor-wrapper {
-          background-color: transparent !important;
-        }
-        .angular-editor-toolbar {
-          border-color: var(--idea-html-editor-border-color, var(--ion-color-light-shade)) !important;
-          background-color: var(--idea-html-editor-toolbar-background-color, var(--ion-color-light)) !important;
-          position: sticky;
-          top: 0;
-          z-index: 1000;
-        }
-        .angular-editor-textarea {
-          background-color: var(--idea-html-editor-background-color, var(--ion-color-light)) !important;
-          border-color: var(--idea-html-editor-border-color, var(--ion-color-light-shade)) !important;
-          color: var(--idea-html-editor-color, var(--ion-color-light-contrast)) !important;
-        }
-        .ae-picker-options,
-        .ae-picker-label {
-          background-color: var(--idea-html-editor-toolbar-background-color, var(--ion-color-light)) !important;
-          border-color: var(--idea-html-editor-toolbar-border-color, var(--ion-color-light)) !important;
-          color: var(--idea-html-editor-toolbar-color, var(--ion-color-light-contrast)) !important;
-        }
-        .angular-editor-button {
-          background-color: var(--idea-html-editor-button-background-color, var(--ion-color-light)) !important;
-          border-color: var(--idea-html-editor-button-border-color, var(--ion-color-light)) !important;
-          color: var(--idea-html-editor-button-color, var(--ion-color-light-contrast)) !important;
-        }
-      }
       div.view {
-        margin: var(--idea-html-editor-margin, 0);
-        padding: var(--idea-html-editor-padding, 20px);
-        background-color: var(--idea-html-editor-background-color, var(--ion-color-white));
-        color: var(--idea-html-editor-color, var(--ion-color-text));
-        box-shadow: var(--idea-html-editor-box-shadow, none);
-        border: var(--idea-html-editor-border-width, 1px) solid
-          var(--idea-html-editor-background-color, var(--ion-border-color));
-        border-radius: var(--idea-html-editor-border-radius, 0);
+        margin: var(--idea-html-editor-view-margin);
+        padding: var(--idea-html-editor-view-padding);
+        background-color: var(--idea-html-editor-view-background-color);
+        color: var(--idea-html-editor-view-color);
+        box-shadow: var(--idea-html-editor-view-box-shadow);
+        border: var(--idea-html-editor-view-border-width) solid var(--idea-html-editor-view-border-color);
+        border-radius: var(--idea-html-editor-view-border-radius);
       }
     `
   ]
 })
 export class IDEAHTMLEditorComponent implements OnInit, OnChanges {
+  private _cdr = inject(ChangeDetectorRef);
   private _sanitizer = inject(DomSanitizer);
   private _alert = inject(AlertController);
   private _translate = inject(IDEATranslationsService);
@@ -127,7 +174,6 @@ export class IDEAHTMLEditorComponent implements OnInit, OnChanges {
     sanitize: true,
     rawPaste: false,
     minHeight: '50vh',
-    // @todo should be an Input configuration
     toolbarHiddenButtons: [['customClasses', 'insertVideo', 'insertHorizontalRule', 'removeFormat', 'toggleEditorMode']]
   };
 
@@ -135,28 +181,41 @@ export class IDEAHTMLEditorComponent implements OnInit, OnChanges {
 
   ngOnInit(): void {
     this.text = this.content;
+    this.sanitizedHtml = this._sanitizer.sanitize(SecurityContext.HTML, this.content);
   }
   ngOnChanges(changes: SimpleChanges): void {
-    if (changes.editMode) this.sanitizedHtml = this._sanitizer.sanitize(SecurityContext.HTML, this.content);
-    if (changes.content) this.sanitizedHtml = this._sanitizer.sanitize(SecurityContext.HTML, this.content);
+    if (changes.content) {
+      this.text = this.content;
+      this.sanitizedHtml = this._sanitizer.sanitize(SecurityContext.HTML, this.content);
+      this._cdr.markForCheck();
+    }
+    if (changes.editMode) this._cdr.markForCheck();
+  }
+
+  onTextChange(text: string): void {
+    this.text = text;
+    this.contentChange.emit(text);
+    this._cdr.markForCheck();
   }
 
   cleanHTMLCode(): void {
     setTimeout((): void => {
       this.text = docsSoap(this.text);
       this.contentChange.emit(this.text);
+      this._cdr.markForCheck();
     }, 100);
   }
 
   async askAndApplySizeToSelectedImages(): Promise<void> {
     const applyWidth = (width: number): void => {
-      const imagesSelected = this.getSelectedElementsInEditorWithQuerySelectorAll('img');
+      const imagesSelected = this.getSelectedImageElementsInEditor();
       if (!imagesSelected.length) return;
       imagesSelected.forEach(img => {
         if (width) img.setAttribute('width', String(width));
         else img.removeAttribute('width');
       });
       this.contentChange.emit(this.editor.textArea.nativeElement.innerHTML);
+      this._cdr.markForCheck();
     };
     const header = this._translate._('IDEA_EDITOR.SELECT_SIZE_FOR_IMAGE');
     const inputs: any = [
@@ -172,8 +231,7 @@ export class IDEAHTMLEditorComponent implements OnInit, OnChanges {
     const alert = await this._alert.create({ header, inputs, buttons });
     alert.present();
   }
-
-  private getSelectedElementsInEditorWithQuerySelectorAll(selectors: string): Element[] {
+  private getSelectedImageElementsInEditor(selectors = 'img'): Element[] {
     const selection = window.getSelection();
     if (!selection?.rangeCount) return [];
     const range = selection.getRangeAt(0);
@@ -194,26 +252,19 @@ export class IDEAHTMLEditorComponent implements OnInit, OnChanges {
     event.preventDefault();
     event.stopPropagation();
   }
-
   onDrop(event: DragEvent): void {
     event.preventDefault();
     event.stopPropagation();
-
     const files = event.dataTransfer?.files;
-
     if (files && files.length > 0) {
       const file = files[0];
-
       if (file.type.startsWith('image/')) {
         const reader = new FileReader();
-
         reader.onload = e => {
           const imgSrc = e.target?.result as string;
-
           this.moveCursorToDropPosition(event);
           this.insertImageAtLastDropPosition(imgSrc);
         };
-
         reader.readAsDataURL(file);
       } else this._message.warning('IDEA_EDITOR.FILE_NOT_IMAGE');
     }
@@ -226,24 +277,29 @@ export class IDEAHTMLEditorComponent implements OnInit, OnChanges {
       this.lastDropPosition.insertNode(imgElement);
       this.lastDropPosition = null;
 
-      const editor = document.querySelector('angular-editor div[contenteditable="true"]') as HTMLElement;
+      const editor = this.editor?.textArea?.nativeElement as HTMLElement;
       const imagesSelected = editor.querySelectorAll('img');
       if (imagesSelected.length === 1) imagesSelected[0].removeAttribute('width');
       this.contentChange.emit(this.editor.textArea.nativeElement.innerHTML);
+      this._cdr.markForCheck();
     }
   }
-
-  moveCursorToDropPosition(event: DragEvent): void {
-    const editor = document.querySelector('angular-editor div[contenteditable="true"]') as HTMLElement;
-
-    if (editor) {
-      editor.focus();
-
-      const x = event.clientX;
-      const y = event.clientY;
-
-      const range = document.caretRangeFromPoint(x, y); // @todo replace deprecated instruction
-      if (range) this.lastDropPosition = range;
-    }
+  private moveCursorToDropPosition(event: DragEvent): void {
+    const root = this.editor?.textArea?.nativeElement as HTMLElement;
+    if (!root) return;
+    root.focus();
+    const range =
+      (document as any).caretRangeFromPoint?.(event.clientX, event.clientY) ??
+      ((): Range => {
+        const pos = (document as any).caretPositionFromPoint?.(event.clientX, event.clientY);
+        if (pos) {
+          const r = document.createRange();
+          r.setStart(pos.offsetNode, pos.offset);
+          r.collapse(true);
+          return r;
+        }
+        return null;
+      })();
+    if (range) this.lastDropPosition = range;
   }
 }
